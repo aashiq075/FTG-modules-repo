f6c849fdf2cf3fe3e7d347bd2d8f93e98266561a
aashiq075 <67676774+aashiq075@users.noreply.github.com>
2020-09-16 14:52:50 +0530

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,77 +1,45 @@
-#    Friendly Telegram (telegram userbot)
-#    Copyright (C) 2018-2019 The Authors
+# For The-TG-Bot v3
+# Syntax .tr <language_name>
 
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-# requires: pyyandextranslateapi
-
-import logging
-from Yandex import Translate
-
-from .. import loader, utils
-
-logger = logging.getLogger(__name__)
+import emoji
+from googletrans import Translator
 
 
-@loader.tds
-class TranslateMod(loader.Module):
-    """Translator"""
-    strings = {"name": "Translator",
-               "translated": "<b>From: </b><code>{from_lang}</code>"
-               "\n<b>To: </b><code>{to_lang}</code>\n\n{output}",
-               "invalid_text": "Invalid text to translate",
-               "doc_default_lang": "Language to translate to by default",
-               "doc_api_key": "API key from https://translate.yandex.com/developers/keys"}
+@client.on(events("tr ?(.*)"))
+async def handler(event):
+    if event.fwd_from:
+        return
+    if "trim" in event.raw_text:
+        return
+    args = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        text = reply_message.message
+        lang = args or "en"
+    elif "|" in args:
+        lang, text = args.split("|")
+    else:
+        await event.edit("`.tr language code` as reply to a message")
+        return
+    text = emoji.demojize(text.strip())
+    lang = lang.strip()
+    translator = Translator()
+    try:
+        try:
+            translated = translator.translate(text, dest=lang)
+        except ValueError:
+            return await event.edit(f"`Language code {lang} is invalid.`")
+        output_str = f"**Translated** from {translated.src} to {lang}:\n`{translated.text}`"
+        await event.edit(output_str)
+    except Exception as ex:
+        await event.edit("`Looks like the google translator library is down, please try again later.`")
+        logger.info(str(ex))
 
-    def __init__(self):
-        self.config = loader.ModuleConfig("DEFAULT_LANG", "en", lambda m: self.strings("doc_default_lang", m),
-                                          "API_KEY", "", lambda m: self.strings("doc_api_key", m))
 
-    def config_complete(self):
-        self.tr = Translate(self.config["API_KEY"])
-
-    @loader.unrestricted
-    @loader.ratelimit
-    async def translatecmd(self, message):
-        """.translate [from_lang->][->to_lang] <text>"""
-        args = utils.get_args(message)
-
-        if len(args) == 0 or "->" not in args[0]:
-            text = " ".join(args)
-            args = ["", self.config["DEFAULT_LANG"]]
-        else:
-            text = " ".join(args[1:])
-            args = args[0].split("->")
-
-        if len(text) == 0 and message.is_reply:
-            text = (await message.get_reply_message()).message
-        if len(text) == 0:
-            await utils.answer(message, self.strings("invalid_text", message))
-            return
-        if args[0] == "":
-            args[0] = self.tr.detect(text)
-        if len(args) == 3:
-            del args[1]
-        if len(args) == 1:
-            logging.error("python split() error, if there is -> in the text, it must split!")
-            raise RuntimeError()
-        if args[1] == "":
-            args[1] = self.config["DEFAULT_LANG"]
-        args[0] = args[0].lower()
-        logger.debug(args)
-        translated = self.tr.translate(text, args[1], args[0])
-        ret = self.strings("translated", message).format(from_lang=utils.escape_html(args[0]),
-                                                         to_lang=utils.escape_html(args[1]),
-                                                         output=utils.escape_html(translated))
-        await utils.answer(message, ret)
+ENV.HELPER.update({
+    "translate": "\
+```.tr <language_code>``` (as a reply to target message)\
+\nUsage: Translate target message to another language.\
+\nClick [here](https://meta.wikimedia.org/wiki/Template:List_of_language_names_ordered_by_code) to see a detailed list of all language codes.\
+"
+})
