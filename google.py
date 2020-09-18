a8202741dd404448ebdbfedbd85e10277839f30f
aashiq075 <67676774+aashiq075@users.noreply.github.com>
2020-09-18 11:24:27 +0530

    Update google.py

--- google.py
+++ google.py
@@ -14,45 +14,65 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# requires: search-engine-parser>=0.5.3
+import os
+import shutil
+from pathlib import Path
 
-import logging
+from userge.utils import runcmd
+from userge import userge, Message
+from userge.plugins.misc.upload import audio_upload
 
-from search_engine_parser import GoogleSearch
-
-from .. import loader, utils
-
-logger = logging.getLogger(__name__)
+TEMP_DIR = "spotdl/"
 
 
-@loader.tds
-class GoogleSearchMod(loader.Module):
-    """Make a Google search, right in your chat!"""
-    strings = {"name": "Google Search",
-               "no_term": "<b>I can't Google nothing</b>",
-               "no_results": "<b>Could not find anything about</b> <code>{}</code> <b>on Google</b>",
-               "results": "<b>These came back from a Google search for</b> <code>{}</code>:\n\n",
-               "result": "<a href='{}'>{}</a>\n\n<code>{}</code>\n"}
+@userge.on_cmd("spotdl", about={
+    'header': "Spotify Downloader",
+    'description': "Download Songs via Spotify Links"
+                   " or just by giving song names. ",
+    'usage': "{tr}spotdl [Spotify Link or Song Name]|[Quality (optional)]",
+    'examples': "{tr}spotdl https://open.spotify.com/track/0Cy7wt6IlRfBPHXXjmZbcP|flac"})
+async def spotify_dl(message: Message):
+    if not os.path.exists(TEMP_DIR):
+        os.makedirs(TEMP_DIR)
+    await message.edit("Checking? 🧐😳🤔🤔")
+    cmd = ''
+    link = ''
+    song_n = ''
+    quality = "mp3"
+    if "|" in message.input_str:
+        input_, quality = message.input_str.split("|")
+        if 'spotify.com' in input_:
+            await message.edit("Link, Hmm, Make sure to give valid one")
+            link = input_
+        else:
+            await message.edit("🤔Song? Searching...")
+            song_n = input_
+    else:
+        input_ = message.input_str
+        if 'spotify.com' in input_:
+            await message.edit("Link, Hmm, Make sure to gave valid one")
+            link = input_
+        else:
+            await message.edit("🤔Song? Searching....")
+            song_n = input_
 
-    @loader.unrestricted
-    @loader.ratelimit
-    async def googlecmd(self, message):
-        """Shows Google search results."""
-        text = utils.get_args_raw(message.message)
-        if not text:
-            text = (await message.get_reply_message()).message
-        if not text:
-            await utils.answer(message, self.strings("no_term", message))
+    if song_n or link:
+        if 'track/' in link:
+            song_n = link
+        if not song_n:
+            await message.edit("Selling Brain is not yet Legalized")
             return
-        # TODO: add ability to specify page number.
-        gsearch = GoogleSearch()
-        gresults = await gsearch.async_search(text, 1)
-        if not gresults:
-            await utils.answer(message, self.strings("no_results", message).format(text))
-            return
-        msg = ""
-        results = zip(gresults["titles"], gresults["links"], gresults["descriptions"])
-        for result in results:
-            msg += self.strings("result", message).format(utils.escape_html(result[0]), utils.escape_html(result[1]),
-                                                          utils.escape_html(result[2]))
-        await utils.answer(message, self.strings("results", message).format(utils.escape_html(text)) + msg)
+        await message.edit("Downloading")
+        quality = quality.strip()  # Just for Precautions 🤷‍♂
+        cmd = f"spotdl --song {song_n} -o {quality} -f {TEMP_DIR}"
+    if cmd:
+        stdout, stderr = (await runcmd(cmd))[:2]
+        if not os.path.lexists(TEMP_DIR):
+            await message.err("Download Failed")
+            raise Exception(stdout + stderr)
+        if os.path.lexists(TEMP_DIR):
+            await message.delete()
+            for track in os.listdir(TEMP_DIR):
+                track_loc = TEMP_DIR + track
+                await audio_upload(message, Path(track_loc), True)
+    shutil.rmtree(TEMP_DIR, ignore_errors=True)
